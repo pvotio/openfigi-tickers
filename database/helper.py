3eb571f195cb105f9bc484979ec915d600f7214c
Ali Moghimi <mghmiali@gmail.com>
2025-04-29 08:32:16 +0000

    feat: load queries from env variables

--- database/helper.py
+++ database/helper.py
@@ -1,3 +1,4 @@
+from config import settings
 from database import MSSQLDatabase
 
 
@@ -7,37 +8,7 @@ def init_db_instance():
 
 def get_ishares():
     conn = init_db_instance()
-    query = """
-    SELECT sub.*
-    FROM (
-        SELECT
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY ISIN, ishares_exchange_name
-            ORDER BY (SELECT NULL)
-            -- if you have a “latest” column, e.g. load_date, use:
-            -- ORDER BY load_date DESC
-        ) AS rn
-        FROM etl.ishares_index_components
-        WHERE ishares_index IN (
-        'iShares Core MSCI International Developed Markets ETF',
-        'iShares Russell 3000 ETF',
-        'iShares Core S&P Total U.S. Stock Market ETF',
-        'iShares MSCI Germany ETF',
-        'iShares MSCI Germany Small-Cap ETF',
-        'iShares Core DAX® UCITS ETF (DE)',
-        'iShares MDAX® UCITS ETF (DE)',
-        'iShares Core SPI® ETF (CH)',
-        'iShares Core MSCI Total International Stock ETF',
-        'iShares Core MSCI Japan IMI UCITS ETF',
-        'iShares MSCI Global Metals & Mining Producers ETF',
-        'iShares MSCI World Islamic UCITS ETF',
-        'iShares MSCI World ETF',
-        'iShares MSCI Emerging Markets ETF'
-        )
-    ) AS sub
-    WHERE sub.rn = 1;
-    """
+    query = settings.ISHARES_QUERY
     table = conn.select_table(query)
     return table
 
@@ -45,17 +16,9 @@ def get_ishares():
 def get_exchanges(comp=False):
     conn = init_db_instance()
     if comp:
-        query = """
-        SELECT ishares_exchange_name, exchange_priority, bbg_exch_comp
-        FROM mdd.Exchanges
-        WHERE ishares_exchange_name IS NOT NULL
-        """
+        query = settings.EXCHANGES_COMP_QUERY
     else:
-        query = """
-        SELECT ishares_exchange_name, exchange_priority, bbg_exch
-        FROM mdd.Exchanges
-        WHERE ishares_exchange_name IS NOT NULL
-        """
+        query = settings.EXCHANGES_QUERY
 
     table = conn.select_table(query)
     records = [list(data.values()) for data in table.to_dict("records")]
