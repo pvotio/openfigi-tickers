f4a19bb2050befcceeb13b296923497e01841b37
Ali Moghimi <mghmiali@gmail.com>
2025-04-29 08:16:41 +0000

    feat: request helper

---
+++ engine/request.py
@@ -0,0 +1,13 @@
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+
+def init_session(token, max_retries, backoff_factor):
+    session = requests.Session()
+    retries = Retry(
+        total=max_retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=[500, 502, 503, 504],
+    )
+    session.mount("https://api.openfigi.com", HTTPAdapter(max_retries=retries))
+    return session
